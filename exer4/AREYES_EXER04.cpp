4ebadecfdb14de93f82bfc2840c7b68a37afa62e
Alex Reyes <akosialexpo@gmail.com>
2013-09-07 03:47:12 +0800

    Finished pal, pass, quit funcs

--- exer4/AREYES_EXER04.cpp
+++ exer4/AREYES_EXER04.cpp
@@ -4,7 +4,7 @@
 * Terminal No: 33
 * Program: BSCS
 * Course / Section: CS127L/AT2
-* Purpose: This program will calculate the speed from distance over time
+* Purpose: 
 */
 
 #include<iostream>
@@ -15,9 +15,10 @@
 using namespace std;
 
 #ifdef __WIN32
-#include<conio.h> //using curses.h
-#include<windows.h> //Need this! Find linux version
+#include<conio.h> //using curses.h instead
+#include<windows.h> //using unistd.h instead
 #endif
+
 #ifdef __linux
 #include <curses.h>
 #include <unistd.h>
@@ -29,8 +30,12 @@ void palindrome(char sal[120]);
 void password();
 char menu();
 void quit();
-
 string EnterPassword();
+
+//User defined global variables
+const string OPTIONA = "[a] Check the palindrome\n";
+const string OPTIONB = "[b] Read in a line of text\n";
+bool authorized = false;
 
 
 int main() //change back to void type
@@ -38,19 +43,91 @@ int main() //change back to void type
 //add all your code here
     char choice;
 
-    password();
+    if (!authorized)
+        password();
 
-    while (true) {
+    while (true) { // Main program loop
         try {
             choice = menu();
             
             switch(choice) {
                 case 'a':
-                        break;
+                    while (true) { // 1: main loop for palindrome
+                        char input[120];
+                        
+                        while (true) { // 2: loop for valid input of string
+                            cout << OPTIONA
+                                 << "Input the word: ";
+
+                            //Check if string is alphanumeric
+                            try {
+                                cin.ignore();
+                                cin.get(input, 120);
+
+                                cout << "input: " << input << endl;
+
+                                int i = 0;
+                                while (input[i] != '\0') { 
+                                    if (!isalnum(input[i])) {
+                                        throw input[i];
+                                    }
+
+                                    i += 1;
+                                }
+                                
+                                break; // out of loop 2
+                            }
+                            catch(char x) {
+                                #ifdef _WIN32
+                                system("CLS");
+                                #endif
+                                cerr << "Invalid character: " << x << "."
+                                     << "Alphanumeric characters only.\n"
+                                     << "Restoring input stream.\n";
+                                cin.clear();
+                                cin.ignore(100, '\n');
+                            }
+                        }
+
+                        palindrome(input); // Check if palindrome
+                        #ifdef _WIN32
+                        system("pause");
+                        #endif
+
+                        // Ask user to try again
+                        #ifdef _WIN32
+                        system("CLS");
+                        #endif
+                        char again;
+                        while (true) { // 3: loop for valid input --> again
+                            cout << "Do you want to try again [y/n]\n"
+                                 << "Choice: ";                                
+
+                            try {
+                                cin >> again;
+
+                                switch (tolower(again)) {
+                                    case 'y':
+                                        break; // out of switch
+                                    case 'n':
+                                        main();
+                                    default:
+                                        throw again;
+                                }
+                                break; // out of loop 3
+                            }
+                            catch(char) {
+                                cerr << "Invalid input.\n";
+                                cin.clear();
+                                cin.ignore(100, '\n');
+                            }
+                        } // End ask user to try again
+                    }
+                    break;
                 case 'b':
-                        break;
+                    break;
                 case 'c':
-                        quit();
+                    quit();
                 default:
                     throw choice;
             }
@@ -77,6 +154,33 @@ void palindrome(char sal[120])
 {
 //add all your code here
 
+    #ifdef _WIN32
+    system("CLS");
+    #endif
+
+    cout << "Testing input...\n";
+
+    cout << sal;
+
+    int length = 0;
+    for (length = 0; length < 120; length++) {
+        if (sal[length] == '\0')
+            break;
+    }
+
+    int i = 0, j = length-1;
+    while (i != j) {
+        if (sal[i] != sal[j]) {
+            cout << sal << " is not a palindrome.\n";
+            return;
+        }
+
+        i += 1;
+        j -= 1;
+    }
+
+    cout << sal << " is a palindrome!\n";
+
 }
 void password()
 {
@@ -91,24 +195,33 @@ void password()
     printw("Enter password\n> "); //change to conio.h
 
     string pass;
-    int counter = 0;
+    int attempts = 0;
 
-    while (counter < 3) {
+    while (true) {
 
         pass = EnterPassword();
 
-        if (pass == "k1udgedit") {
-            printw("Welcome. Loading Main Menu"); //change to conio.h
+        printw("Processing password"); //change to conio.h
 
-            for (int i = 0; i < 5; i++) {
-                #ifdef _WIN32
-                Sleep(700);
-                #endif
-                #ifdef _linux
-                usleep(1);
-                #endif
-                printw(".");
-            }
+        for (int i = 0; i < 5; i++) {
+            #ifdef _WIN32
+            Sleep(700);
+            #endif
+            
+            printw(".");
+        }
+
+        if (pass == "password") {
+            #ifdef _WIN32
+            system("CLS");
+            #endif
+            cout << "Accepted.\n"
+                 << "Program initiating...\n";
+            authorized = true;
+            #ifdef _WIN32
+            Sleep(1300);
+            #endif
+            
             break;
         }
         else {
@@ -116,15 +229,16 @@ void password()
             system("CLS");
             #endif
             printw("Incorrect. Try again.\n> "); //change to conio.h
-            counter += 1;
+            attempts += 1;
         }
 
-        if (counter >= 3) {
+        if (attempts >= 3) {
             cout << "Too many failed attempts. Exiting program...\n\n";
             #ifdef _WIN32
             Sleep(1000);
             exit(0);
             #endif
+            break; //Remove this
         }
 
 
@@ -145,9 +259,9 @@ char menu()
 
     cout << "========== Menu ==========\n"
          << "  Operations: \n"
-         << "\t [a] Something \n"
-         << "\t [b] Something \n"
-         << "\t [c] Something \n"
+         << "\t" << OPTIONA
+         << "\t" << OPTIONB
+         << "\t[c] Quit \n"
          << "> ";
     cin >> option;
 
@@ -171,8 +285,10 @@ void quit()
                 throw confirm;
             else if (confirm == 'y' || confirm == 'Y')
                 exit(0);
-            else
+            else if (confirm == 'n' || confirm == 'N')
                 main();
+            else
+                throw confirm;
         }
         catch(char) {
             #ifdef _WIN32
@@ -197,7 +313,7 @@ string EnterPassword()
     while ((key = getch()) != RETURN) {
         
         if (key == BACKSPACE) {
-            if(entry.length() >= 0) {
+            if(entry.length() > 0) {
                 printw("\b \b"); //Needs lab testing
                 entry.resize(entry.size()-1);
             }   
